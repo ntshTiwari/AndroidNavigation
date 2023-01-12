15590502f89ec4fea03a90c5c050d44c537cdc66
Nitesh Kumar <niteshk@geekyants.com>
2023-01-12 18:29:13 +0530

    feat: Added navigation_graph, NavHostFragment and empty fragments

---
+++ app/src/main/java/com/example/androidnavigation/noteFragment.kt
@@ -0,0 +1,17 @@
+package com.example.androidnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class noteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+}
